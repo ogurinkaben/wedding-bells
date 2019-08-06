51e9b3758cf721e6968452fa8765346724b871cd
simplytammy <ogurinkabenjamin@gmail.com>
2019-08-06 02:36:41 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -1,35 +1,42 @@
+$(document).ready(function() {
+  // Add smooth scrolling 
+  $("a.scroll").on('click', function(event) {
 
-    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-        var smoothScroll = function (anchor, duration) {
-            var startLocation = window.pageYOffset;
-            var endLocation = anchor.offsetTop;
-            var distance = endLocation - startLocation;
-            var increments = distance/(duration/16);
-            var stopAnimation;
-            var animateScroll = function () {
-                window.scrollBy(0, increments);
-                stopAnimation();
-            };
-            if ( increments >= 0 ) {
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
-                    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-                        clearInterval(runAnimation);
-                    }
-                };
-            }
-            var runAnimation = setInterval(animateScroll, 16);
-        };
-        var scrollToggle = document.querySelectorAll('.scroll');
-        [].forEach.call(scrollToggle, function (toggle) {
-            toggle.addEventListener('click', function(e) {
-                e.preventDefault();
-                var dataID = toggle.getAttribute('href');
-                var dataTarget = document.querySelector(dataID);
-                var dataSpeed = toggle.getAttribute('data-speed');
-                if (dataTarget) {
-                    smoothScroll(dataTarget, dataSpeed || 500);
-                }
-            }, false);
-        });
+    if (this.hash !== "") {
+      event.preventDefault();
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
     }
+
+});
+
+  //carousel
+      $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+            "<i class='ti-arrow-circle-left'></i>",
+            "<i class='ti-arrow-circle-right'></i>"
+        ],
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
