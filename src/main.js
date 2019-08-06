e43a5842a834d4055fa17a86ee0789cad8fffe59
simplytammy <ogurinkabenjamin@gmail.com>
2019-08-06 11:49:18 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
-  // Add smooth scrolling 
-  $("a.scroll").on('click', function(event) {
+    $('.seat-saved-msg').hide();
 
-    if (this.hash !== "") {
-      event.preventDefault();
-      const hash = this.hash;
+    setTimeout(function() {
+        $('.loader').fadeOut();
+    }, 100);
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    }
 
-});
+    // Add smooth scrolling 
+    $("a.scroll").on('click', function(event) {
 
-  //carousel
-      $('.owl-carousel').owlCarousel({
+        if (this.hash !== "") {
+            event.preventDefault();
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+
+    });
+
+    //carousel
+    $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
@@ -39,4 +46,63 @@ $(document).ready(function() {
             }
         }
     });
+    //Client Side Form Validatiom
+    const main = function() {
+        $('form').submit(function() {
+            const fullName = $('#name').val();
+
+            if (fullName === "") {
+                $('.name-error').text('Please enter your first name.').fadeIn();;
+                setTimeout(function() {
+                    $('.name-error').text('Please enter your first name.').fadeOut();;
+                }, 3000);
+
+            }
+
+            return false;
+        });
+
+        $('form').submit(function() {
+            const info = $('#contactInfo').val();
+
+            if (info === "") {
+                $('.info-error').text('Please enter your first name.').fadeIn();
+
+                setTimeout(function() {
+                    $('.info-error').text('Please enter your first name.').fadeOut();
+                }, 3000);
+            };
+            return false;
+        });
+        $('form').submit(function() {
+
+            let contactInfo = $('#contactInfo').val();
+            let name = $('#name').val();
+            if (contactInfo !== "" && name !== "") {
+                $('.seat-saved-msg').fadeIn();
+                $('.seat-saved-msg h1').text('See you there ' + name).fadeIn();
+                setTimeout(function() {
+
+                    $('.seat-saved-msg').fadeOut();
+                    $('.seat-saved-msg h1').text('See you there ' + name).fadeOut();
+                    location.reload();
+                    $('form').find("input[type=text], textarea").val("");
+                }, 3000);
+
+            }
+            return true;
+
+
+        });
+
+    }
+
+    $(document).ready(main);
+
+    lightbox.option({
+    'albumLabel':   "picture %1 of %2",
+    'fadeDuration': 300,
+    'resizeDuration': 150,
+    'wrapAround': true
+})
 });
